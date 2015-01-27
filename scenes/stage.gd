e266329b19e3168f9b30d8ba196004be1fd48110
Randy Yang <randyyaj1@gmail.com>
2015-01-27 01:15:40 -0500

    Significant improvements to platforms. Platforms should mostly work with little bug when hitting platform side. Added new bg track. Figured out how to do multiple simultaneos music track with SamplePlayer2D. Pretty intuitive stuff setting the polyphony on the properties menu.

--- scenes/stage.gd
+++ scenes/stage.gd
@@ -24,6 +24,7 @@ var is_ui_cancel_pressed = false
 var is_ui_accept_pressed = false
 var is_player_in_air = false
 var is_platform_movable = true
+var is_new_game = true
 
 # Global vars ------------------------------------------------------------------
 var player
@@ -32,6 +33,8 @@ var music
 var player_anim
 var current_platform
 var previous_platform
+var next_platform
+var future_platform
 var charge_meter
 var lava
 var scene
@@ -46,26 +49,28 @@ func _ready():
 	player_anim = player.get_node('playerSprite/playerAnimation')
 	lava = get_node('death')
 	music = get_node('SamplePlayer2D');
-	generate_base_platform()
-	generate_random_platform()
+	music.play('bg',0)
+	next_platform = generate_random_platform()
+	current_platform = generate_base_platform()
+	future_platform = generate_random_platform()
 	set_process(true)
 
 
-func _process(delta):
-	var player_pos = player.get_pos()
-	var lava_pos = lava.get_pos()
+func _process(delta):	
 	
-	if (previous_platform != null):
-		previous_platform.move(Vector2(0,0))
+	if (!music.is_voice_active(0)):
+		music.play('bg', 0)
 	
-	if (is_platform_movable):
+	if (current_platform != null):
 		current_platform.move(Vector2(0,0))
 	
-	if (current_platform.is_colliding()):
+	if (is_platform_movable):
+		next_platform.move(Vector2(0,0))
+	
+	if (next_platform.is_colliding()):
 		reset_position(delta)
 	
-	if (player_pos.y > lava_pos.y):
-		print("DEAD")
+	if (player.get_pos().y > lava.get_pos().y):
 		reset_game()
 		#TODO prompt game over retry
 	
@@ -98,7 +103,7 @@ func launch_player(charge):
 		charge = MAX_CHARGE
 		
 	player_anim.play('fly')
-	music.play('Jump')
+	music.play('Jump', 1)
 	player.set_linear_velocity(Vector2(charge, -(charge * 2)))
 
 
@@ -108,34 +113,41 @@ func reset_position(delta):
 	charge_meter.set_value(0)
 	player.set_linear_velocity(Vector2(0,0))
 	
-	if (current_platform.get_pos().x > 35):
+	if (next_platform.get_pos().x > 35):
 		player.set_pos(Vector2(player.get_pos().x - 400 * delta, player.get_pos().y))
 		current_platform.set_pos(Vector2(current_platform.get_pos().x - 400 * delta, current_platform.get_pos().y))
-		previous_platform.set_pos(Vector2(previous_platform.get_pos().x - 400 * delta, previous_platform.get_pos().y))
+		next_platform.set_pos(Vector2(next_platform.get_pos().x - 400 * delta, next_platform.get_pos().y))
+		future_platform.set_pos(Vector2(future_platform.get_pos().x - 400 * delta, future_platform.get_pos().y))
 	else:
 		is_platform_movable = true 
 		score.increment()
-		generate_random_platform()
-		#previous_platform.free()
-		#previous_platform = current_platform
+		previous_platform = current_platform
+		current_platform = next_platform
+		next_platform = future_platform
+		future_platform = generate_random_platform()
+		previous_platform.free()
+		previous_platform = null
 
 
 func reset_game():
-	charge_meter.set_value(charge)
+	is_new_game = true
 	
-	if (previous_platform != null):
+	if(previous_platform != null):
 		previous_platform.free()
-	
+
 	if (current_platform != null):
 		current_platform.free()
 	
+	if (next_platform != null):
+		next_platform.free()
+	
 	is_platform_movable = true
-	generate_base_platform()
-	generate_random_platform()
+	current_platform = generate_base_platform()
+	next_platform = generate_random_platform()
 	player.set_pos(Vector2(70, 490))
 	player.set_rot(0)
 	player.set_linear_velocity(Vector2(0,0))
-	previous_platform.set_pos(Vector2(35, 510))
+	charge_meter.set_value(0)
 	score.reset()
 	
 
@@ -150,11 +162,15 @@ func get_random_range(begin, end):
 
 
 func generate_random_platform():
-	var x_coordinate = get_random_range(200,680)
+	var x_coordinate = get_random_range(800, 950)
 	var y_coordinate = 500
 	var random = get_random(4)
 	var scene = load(PLATFORMS[random])
 	var node = scene.instance()
+	
+	if (is_new_game):
+		x_coordinate = get_random_range(200,680) 
+		is_new_game = false
 	
 	if (random == 2):
 		y_coordinate = 520
@@ -164,12 +180,14 @@ func generate_random_platform():
 	node.set_scale(Vector2(3,3))
 	#node.set_z(1)
 	move_child(node, 0)
-	current_platform = node
-	current_platform.set_collide_with_rigid_bodies(true)
-	current_platform.set_collide_with_character_bodies(true)
+	
+	node.set_collide_with_rigid_bodies(true)
+	node.set_collide_with_character_bodies(true)
+	
+	return node
 	
 	
-func generate_base_platform():	
+func generate_base_platform():
 	var random = get_random(4)
 	var scene = load(PLATFORMS[random])
 	var node = scene.instance()
@@ -178,10 +196,9 @@ func generate_base_platform():
 	node.set_pos(Vector2(20, 510))
 	node.set_scale(Vector2(3,3))
 	move_child(node, 0)
-	previous_platform = node
-	previous_platform.set_collide_with_rigid_bodies(true)
-	previous_platform.set_collide_with_character_bodies(true)
-	
+	node.set_collide_with_rigid_bodies(true)
+	node.set_collide_with_character_bodies(true)
+	return node
 	
 
 #func prompt_game_over:
